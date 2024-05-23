bd544032a313478b888babc271634d90277173c5
AstieOCE <150226252+AstieOCE@users.noreply.github.com>
2024-05-23 20:02:23 +0930

    Added Recording Settings. Non-Functional.

---
+++ src/main/java/com/astieoce/divinewhisper/camera/RecordingSettings.java
@@ -0,0 +1,38 @@
+package com.astieoce.divinewhisper.camera;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RecordingSettings {
+    private final Map<String, Object> settings = new HashMap<>();
+
+    public RecordingSettings() {
+        // Default settings
+        settings.put("playbackSpeed", 1);
+        settings.put("relativePosition", false);
+        settings.put("enableGravity", false);
+        settings.put("gamemode", "spectator");
+
+        // Default Recording Settings
+        settings.put("recordInteracts", true);
+        settings.put("recordTime", false);
+        settings.put("recordInventory", false);
+        settings.put("recordGamemode", false);
+        settings.put("recordState", false);
+        settings.put("recordSelfSkin", false);
+        settings.put("recordEntities", false);
+        settings.put("recordSounds", false);
+    }
+
+    public Object getSetting(String key) {
+        return settings.get(key);
+    }
+
+    public void setSetting(String key, Object value) {
+        settings.put(key, value);
+    }
+
+    public Map<String, Object> getAllSettings() {
+        return settings;
+    }
+}
